acbe7b27961b978f042b51d1f8cebad2d3fc0589
Antoine Wurtz <antoine.wurtz@crowdaa.com>
2024-12-27 13:34:27 +0100

    start

--- functions.php
+++ functions.php
@@ -1,6 +1,7 @@
 <?php
 
-add_action ('wp_head', 'tailwind_styles');
-function tailwind_styles(){
-    wp_enqueue_style('perso-creatisweb', get_stylesheet_directory_uri().'/style.css', array(), '1.0.0');
+add_action('wp_head', 'tailwind_styles');
+function tailwind_styles()
+{
+    wp_enqueue_style('perso-creatisweb', get_stylesheet_directory_uri() . '/style.css', array(), '1.0.0');
 }
--- index.php
+++ index.php
@@ -1,14 +1,17 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Test Intégrateur WP</title>
     <?php wp_head(); ?>
 </head>
+
 <body>
-<?php the_title('<h1 class="text-3xl font-bold">','</h1>'); ?>
-    
-<?php wp_footer(); ?>
+    <?php the_title('<h1 class="text-3xl font-bold bg-blue-300">', '</h1>'); ?>
+
+    <?php wp_footer(); ?>
 </body>
+
 </html>
